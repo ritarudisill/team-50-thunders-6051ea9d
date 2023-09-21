84014997e59957dc95658b4a339edd46d07eaacf
ritarudisill <132368330+ritarudisill@users.noreply.github.com>
2023-09-21 11:51:07 -0400

    Create CharacterLibrary.py

---
+++ tests/robot/CharacterLibrary.py
@@ -0,0 +1,5 @@
+class TestCharacterInitWithName(TestCase):
+    def test_init(self):
+        DEFAULT_NAME = "MyName"
+        testobj = Character(DEFAULT_NAME)
+        self.assertEqual(DEFAULT_NAME, testobj.name)
