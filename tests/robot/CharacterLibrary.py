847a037bb33d573d96e573998fda7aecb614dc10
ritarudisill <132368330+ritarudisill@users.noreply.github.com>
2023-09-21 15:22:29 -0400

    Update CharacterLibrary.py

--- tests/robot/CharacterLibrary.py
+++ tests/robot/CharacterLibrary.py
@@ -1,8 +1,12 @@
 from levelup.character import Character
 
 class TestCharacterInitWithName(Character):
-    def test_init(Character):
+    def test_init_with_default(Character):
         DEFAULT_NAME = "MyName"
         testobj = Character(DEFAULT_NAME)
         self.assertEqual(DEFAULT_NAME, testobj.name)
-        
+
+    def test_init_with_name(Character):
+        DEFAULT_NAME = "MyName"
+        testobj = Character(DEFAULT_NAME)
+        self.assertEqual(DEFAULT_NAME, testobj.name)
