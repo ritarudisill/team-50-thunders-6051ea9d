1f8afa4fa0d323027a8797ecba9970117fba2ce8
thunders <ritarudisill@yahoo.com>
2023-09-22 15:43:40 +0000

    TestCommitY

--- tests/test_GameStatus.py
+++ tests/test_GameStatus.py
@@ -5,8 +5,15 @@ from levelup.character import Character
 
 class GameStatus(TestCase):
     def GameStatus(Character,CurrentPosition,MoveCount):
-        Character: String
-        CurrentPosition: Integer
-        MoveCount: Integer
+        Character: String = DEFAULT_NAME
+        CurrentPosition: tuple = (-100, -100)
+        MoveCount: Integer = 0
         testobj = GameStatus(Character,CurrentPosition,MoveCount)
         self.assertEqual(GameStatus, testobj.GameStatus)
+
+## class GameStatus:
+##   running: bool = False
+##    character_name: str = DEFAULT_CHARACTER_NAME
+##    ## NOTE - Game status will have this as a tuple. The Position should probably be in a class
+##    current_position: tuple = (-100,-100)
+ ##   move_count: int = 0 */
